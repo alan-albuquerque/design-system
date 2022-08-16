00e56d6bd29c030cf1593c9caab72199a0198ceb
Alan Nascimento <alanteclog@gmail.com>
2022-08-16 18:49:55 -0300

    test

--- source/components/layout/ArticlePreview.jsx
+++ source/components/layout/ArticlePreview.jsx
@@ -3,7 +3,7 @@ import Button from "../UI/Button/Button";
 const ArticlePreview = () => {
     return (
         <section>
-            <h1>Title</h1>
+            <h1>Another Title</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis, sapien sit amet rutrum ultricies, mi sapien tempor dui, in maximus est mauris sit amet urna. Sed suscipit ligula sem, nec facilisis eros porttitor non.</p>
             <Button>Read more</Button>
         </section>
