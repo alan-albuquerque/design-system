51365a55dfbfd1b2a6c0ff31329840df26bb9a0c
Alan Nascimento <alanteclog@gmail.com>
2022-08-16 18:37:44 -0300

    add chromatic workflow

--- source/components/layout/Articles.jsx
+++ source/components/layout/Articles.jsx
@@ -2,11 +2,11 @@ import ArticlePreview from "./ArticlePreview";
 
 const Articles = () => {
     return (
-        <>
+        <div style={{padding: '1rem', fontSize: '2rem'}}>
             <ArticlePreview />
             <ArticlePreview />
             <ArticlePreview />
-        </>
+        </div>
     );
 }
 
